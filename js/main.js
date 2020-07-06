ac4d0a720ea6b1282023b030e59b21fb39dce940
Миллер Олеся <56222371+MillerOlesiaJob@users.noreply.github.com>
2020-07-06 17:47:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.fairy-tale__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/back-button.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/next-button.svg" alt=""></button>',
+    autoplay: 3000,
+  });
+});

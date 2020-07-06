4c3523609c43c198ca684f7a8ab17399cf7634b4
Миллер Олеся <56222371+MillerOlesiaJob@users.noreply.github.com>
2020-07-06 19:30:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(function(){
   $('.fairy-tale__slider').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/back-button.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/next-button.svg" alt=""></button>',
-    autoplay: 3000,
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next-button.svg" alt=""></button>',
+    autoplay: true,
+    fade: true,
+  });
+
+  $('.our-trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/back-button.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next-button.svg" alt=""></button>',
+    autoplay: true,
+    fade: true,
   });
 });
